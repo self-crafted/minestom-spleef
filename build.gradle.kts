799f24e99d5fafd3bdf50c5aa9d59ead7e091cfc
offbyone <offbyone@disroot.org>
2022-04-30 19:09:21 +0200

    Add Gradle 7.4.2 and configuration

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    alias(libs.plugins.blossom)
+    alias(libs.plugins.shadowJar)
+    java
+}
+
+var displayName = "MinestomSpleef"
+
+group = "com.github.selfcrafted"
+version = "1.0.0-SNAPSHOT"
+
+dependencies {
+    implementation(libs.minestom)
+
+    compileOnly(libs.bundles.logging)
+}
+
+tasks {
+    blossom {
+        replaceToken("&NAME", displayName.toUpperCase())
+        replaceToken("&Name", displayName)
+        replaceToken("&name", displayName.toLowerCase())
+        replaceToken("&version", version)
+        replaceToken("&minestomVersion", libs.versions.minestom.get())
+    }
+
+    processResources {
+        expand(
+            mapOf(
+                "Name" to displayName,
+                "version" to version
+            )
+        )
+    }
+
+    shadowJar {
+        manifest {
+            attributes("Main-Class" to "com.github.selfcrafted.minestomspleef.Server")
+        }
+        archiveBaseName.set(displayName)
+        archiveClassifier.set("")
+        archiveVersion.set(project.version.toString())
+        mergeServiceFiles()
+    }
+
+    test {
+        useJUnitPlatform()
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
